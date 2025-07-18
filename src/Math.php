65ec1e84dd39fe1695ad811d06e7a7de2de9976f
LuckyWindsck <luckywindsck@gmail.com>
2025-07-19 03:32:32 +0900

    chore: make ecs

--- src/Math.php
+++ src/Math.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 final readonly class Math
 {
-  public static function add(int $a, int $b): int {
-    return $a + $b;
-  }
+    public static function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
 
-  public static function multiply(int $a, int $b): int {
-    return $a * $b;
-  }
+    public static function multiply(int $a, int $b): int
+    {
+        return $a * $b;
+    }
 
-  public static function square(int $a): int {
-    return Math::multiply($a, $a);
-  }
+    public static function square(int $a): int
+    {
+        return Math::multiply($a, $a);
+    }
 }
--- tests/MathTest.php
+++ tests/MathTest.php
@@ -6,41 +6,41 @@ use PHPUnit\Framework\TestCase;
 
 final class MathTest extends TestCase
 {
-  public function test_add(): void
-  {
-    $sum = Math::add(1, 2);
+    public function test_add(): void
+    {
+        $sum = Math::add(1, 2);
 
-    $this->assertSame(3, $sum);
-  }
+        $this->assertSame(3, $sum);
+    }
 
-  public function test_multiply(): void
-  {
-    $product = Math::multiply(2, 3);
+    public function test_multiply(): void
+    {
+        $product = Math::multiply(2, 3);
 
-    $this->assertSame(6, $product);
-  }
+        $this->assertSame(6, $product);
+    }
 
-  public function test_square(): void
-  {
-    $squared = Math::square(4);
+    public function test_square(): void
+    {
+        $squared = Math::square(4);
 
-    $this->assertSame(16, $squared);
-  }
+        $this->assertSame(16, $squared);
+    }
 
-  public function test_square_with_staticmock(): void
-  {
-    $staticmock = StaticMock::mock('Math');
+    public function test_square_with_staticmock(): void
+    {
+        $staticmock = StaticMock::mock('Math');
 
-    $staticmock->shouldReceive('multiply')
-      ->once()
-      ->with(4, 4)
-      ->andReturn(16);
+        $staticmock->shouldReceive('multiply')
+          ->once()
+          ->with(4, 4)
+          ->andReturn(16);
 
-    $squared = Math::square(4);
+        $squared = Math::square(4);
 
-    $staticmock->assert();
-    $this->assertSame(16, $squared);
+        $staticmock->assert();
+        $this->assertSame(16, $squared);
 
-    unset($staticmock);
-  }
+        unset($staticmock);
+    }
 }
